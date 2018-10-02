01ec6566117ab68b480457cbe9b41b8e158b8952
Austin <austinvanderwel16@gmail.com>
2018-10-01 18:19:09 -0700

    added an array database

--- JavaSummer18_projects/src/main/java/studentDatabase/studentDatabaseApp.java
+++ JavaSummer18_projects/src/main/java/studentDatabase/studentDatabaseApp.java
@@ -1,19 +1,32 @@
 package studentDatabase;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class studentDatabaseApp {
 
   public static void main(String[] args) {
 
-    aStudent s1 = new aStudent();
-    s1.enroll();
-    s1.viewAndPay();
-    System.out.println(s1.printStatus());
+
+
     // Ask for the total number of users to add
+    System.out.println("How many students do you want to enroll?");
+    Scanner in = new Scanner(System.in);
+    int totalStudents = in.nextInt();
+
+    aStudent[] students = new aStudent[totalStudents];
 
     // Create n number of given students
-    ArrayList<aStudent> studentList;
+    for (int i = 0; i<= totalStudents - 1; i++) {
+      students[i] = new aStudent();
+      students[i].enroll();
+      students[i].viewAndPay();
+      //System.out.println(students[i].printStatus());
+
+    }
+    for (int j = 0; j<= totalStudents -1; j++) {
+      System.out.println(students[j].printStatus());
+    }
 
   }
 }
